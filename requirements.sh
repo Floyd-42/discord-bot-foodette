0006d73e666613439c051c42cfc54156911832f6
YulioVegas <102596332+YulioVegas@users.noreply.github.com>
2022-03-29 14:42:01 +0200

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+pip3 install bs4
+pip3 install git+https://github.com/Rapptz/discord.py
+pip install python-dotenv
+echo BOT_TOKEN="XX" > .env
+echo "You should modify the BOT_TOKEN into the .env file"
